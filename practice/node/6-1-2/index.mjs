34940ab0c8feb3caf24e422fdb4270db6191ede1
relu <relu00123@gmail.com>
2026-03-14 20:40:10 +0900

    6-1-2 review done

---
+++ practice/node/6-1-2/index.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import http from "http";
+import cors from "cors";
+
+const app = express();
+const PORT = 8000;
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/api/v1/hello-world", (req, res) => {
+  try {
+    return res.json({
+      text: "안녕 Express.js",
+    });
+  } catch (error) {
+    return res.json({
+      error: error.message,
+    });
+  }
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`This server is listening on ${PORT}`));
+
